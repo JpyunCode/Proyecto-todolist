a249585e8d2fd4c03955e625b4f4f2e999aa1763
jhonel <201801252k@utea.edu.pe>
2022-01-23 14:28:08 -0500

    funcion editar tarea

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const inputBox = document.querySelector(".inputField input");
 const addBtn = document.querySelector(".inputField button");
 const todoList = document.querySelector(".todoList");
 const deleteAllBtn = document.querySelector(".footer button");
-
+var numeroTarea;
 inputBox.onkeyup = () => {
   let userEnteredValue = inputBox.value;
   if (userEnteredValue.trim() != 0) {
@@ -44,12 +44,31 @@ function showTasks() {
   }
   let newLiTag = "";
   listArray.forEach((element, index) => {
-    newLiTag += `<li>${element}<span class="icon" onclick="deleteTask(${index})"><i class="bi bi-trash-fill"></i></span></li>`;
+    newLiTag += `<li>${element}<span class="icon" ><i class="bi bi-trash-fill" onclick="deleteTask(${index})">
+    </i><span class="icon2"><i class="bi bi-pencil-fill" onclick="showEditTask(${index})"></i>
+    </span></span></li>`;
   });
   todoList.innerHTML = newLiTag;
   inputBox.value = "";
 }
+function showEditTask(index) {
+  document.getElementsByClassName("fondo_transparente")[0].style.display = "block";
+  numeroTarea = index;
 
+}
+function editTask() {
+  var d = document.getElementById("nuevaTarea").value;
+  console.log(d);
+  let getLocalStorageData = localStorage.getItem("New Todo");
+  listArray = JSON.parse(getLocalStorageData);
+  lenArray = listArray.length;
+  console.log(listArray);
+  listArray[numeroTarea] = d;
+  localStorage.setItem("New Todo", JSON.stringify(listArray));
+
+  console.log(listArray);
+  showTasks();
+}
 function deleteTask(index) {
   let getLocalStorageData = localStorage.getItem("New Todo");
   listArray = JSON.parse(getLocalStorageData);
